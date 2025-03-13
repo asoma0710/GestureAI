c5055542b60ed9d09add822f96b349f48784d4fc
Taris Major <tarismajor@gmail.com>
2025-03-12 21:33:06 -0500

    Added some images and modified the setup.sql file

--- database/setup.sql
+++ database/setup.sql
@@ -20,6 +20,8 @@ CREATE TABLE videos (
     video_name VARCHAR(255) NOT NULL,
     video_path VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
+    FOREIGN KEY (video_id) REFERENCES subtitles(video_id)
+    FOREIGN KEY (video_name) REFERENCES subtitles(video_name)
 );
 
 CREATE TABLE users (
@@ -32,5 +34,26 @@ CREATE TABLE users (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE user_videos (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    video_id INT NOT NULL,
+    video_path VARCHAR(255) NOT NULL,
+    video_type VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (video_id) REFERENCES videos(id)
+)
+
+CREATE TABLE user_images (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    image_path VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)
+
 --We need to load the data into the database
---LOAD DATA INFILE 'C:\Users\taris\Downloads\how2sign_train.csv' INTO TABLE subtitles FIELDS TERMINATED BY '    ' LINES TERMINATED BY '\n' IGNORE 1 LINES;
+--LOAD DATA INFILE 'C:\Users\taris\Downloads\how2sign_train.csv' INTO TABLE gestureai.subtitles FIELDS TERMINATED BY '    ' LINES TERMINATED BY '\n' IGNORE 1 LINES;
